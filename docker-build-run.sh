5ca3eaca05cdd830224d7e6ac8979f4cd8529519
Wesley Cheah <e0026557@u.nus.edu>
2025-12-04 13:36:17 +0800

    Updated for testing

---
+++ docker-build-run.sh
@@ -0,0 +1,2 @@
+docker build -t fastapi-app
+docker run -d -p 8000:8000 fastapi-app
--- regcred.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl create secret docker-registry regcred --from-file=.dockerconfigjson=/Users/Darryl/.docker/config.json -oyaml --dry-run=client > regcred.yaml
